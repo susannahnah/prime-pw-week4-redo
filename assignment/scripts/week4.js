85cff1a5a3e6bf90ea6957df2b73c1a7bd342647
Susannah Harris <harris.susannah@gmail.com>
2019-05-02 11:07:19 -0500

    first save

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -1,1 +1,11 @@
 console.log( 'js' );
+let parkedCars = [];
+//created empty array
+const maxCars = 27
+//created max number of cars allowed
+let freeSpace = true;
+
+function parkCar(){
+  console.log( 'in parking spot' );
+
+}
