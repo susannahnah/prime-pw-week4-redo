04ffd374be25bdbd5f439053d9ef448865e01a1a
Susannah Harris <harris.susannah@gmail.com>
2019-05-02 19:03:12 -0500

    second save

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -1,11 +1,22 @@
 console.log( 'js' );
 let parkedCars = [];
 //created empty array
-const maxCars = 27
+const maxCars = 27;
 //created max number of cars allowed
 let freeSpace = true;
 
-function parkCar(){
-  console.log( 'in parking spot' );
-
+function parkCar(randomCar){
+  if (parkedCars.length < maxCars ){
+    parkedCars.push(randomCar);
+    return true;
+  }
+  else{
+    return false;}
 }
+
+console.log( parkedCars );
+parkCar( 'PT Cruiser' );
+parkCar( 'Truck' );
+parkCar( 'Honda' );
+parkCar( 'Jeep' );
+console.log(parkCar( 'Caddy' ));
