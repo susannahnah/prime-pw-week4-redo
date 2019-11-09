9020a2debe799e16ba003528e1e3e31745ff7a81
Susannah Harris <harris.susannah@gmail.com>
2019-11-09 14:15:51 -0600

    set up assignment without answers

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -1,24 +1,11 @@
-let parkedCars = [];
+
 //created empty array
-const maxCars = 27;
+
+
 //created max number of cars allowed
 
-function parkCar(randomCar){
-  //defining the name of our function
-  if (parkedCars.length < maxCars )
-  //looking at garage space
-  {
-    parkedCars.push(randomCar);
-    //pushing random car into garage if space
-    return true;
-  }
-  else{
-    return false;}
-}
 
-console.log( parkedCars );
-parkCar( 'PT Cruiser' );
-parkCar( 'Truck' );
-parkCar( 'Honda' );
-parkCar( 'Jeep' );
-console.log(parkCar( 'Caddy' ));
+  //defining the name of our function
+  //looking at garage space
+  //pushing random car into garage if space
+
